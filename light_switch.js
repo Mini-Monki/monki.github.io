2f5fab0e7f599d8a295734b5a03dd5e28c79362f
Mini-Monki <zazidius@gmail.com>
2026-02-01 19:26:25 +0100

    Fix color assignment in changeColors function

--- light_switch.js
+++ light_switch.js
@@ -1,6 +1,6 @@
 function changeColors(bg, txt) {
   document.body.style.backgroundColor = bg;
-  document.body.style.color = text;
+  document.body.style.color = txt;
 }
 function sayHi() {
   alert('java script jelen!')
